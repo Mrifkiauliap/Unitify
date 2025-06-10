8290830f9b57874eb148fcd871989b333d6a5421
Muhammad Rifki Aulia Pratama <mrifkiauliap2105@gmail.com>
2025-06-10 23:52:13 +0700

    (Added) Basic LandingPage

---
+++ client/src/components/Card/FooterCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer items-center p-10 bg-base-200 text-base-content">
+    <div class="items-center grid-flow-col">
+      <img src="/unitify.png" alt="Unitify Logo" class="w-10 h-10" />
+      <p class="font-bold">
+        Unitify © 2025 <br />
+        Digital Unitia 4 - All Rights Reserved
+      </p>
+    </div>
+
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <a href="https://github.com/Mrifkiauliap/Unitify" target="_blank" class="hover:opacity-80">
+        <img src="/github-mark.svg" alt="GitHub" class="h-6 w-6" />
+      </a>
+      <a href="" target="_blank" class="hover:opacity-80">
+        <img src="/instagram-mark.svg" alt="Instagram" class="h-6 w-6" />
+      </a>
+    </div>
+  </footer>
+</template>
+
