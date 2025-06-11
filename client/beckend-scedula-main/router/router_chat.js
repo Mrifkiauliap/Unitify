28f422c04ca4b9a7249f7d3a2fd41ec96f702ce6
darma <darmagans349@gmail.com>
2025-06-11 16:57:41 +0700

    salah commit wkwkwk

--- client/beckend-scedula-main/router/router_chat.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../modules/chat/controllers/index.js");
-
-router.get("/daftar-chat", controller.daftar_chat);
-
-module.exports = router;
--- client/beckend-scedula-main/router/router_user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../modules/user/controllers/index.js");
-
-router.get("/daftar-user", controller.daftar_user);
-
-module.exports = router;
